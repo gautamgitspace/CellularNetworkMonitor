b05376d8c39c950517474ff4f9cfea9c7d488e48
Abhishek Gautam <agautam2@buffalo.edu>
2016-06-18 18:24:40 -0400

    added code for DB

--- app/src/main/java/ubcomputerscience/ubwins/cellularnetworkmonitor/MainActivity.java
+++ app/src/main/java/ubcomputerscience/ubwins/cellularnetworkmonitor/MainActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
