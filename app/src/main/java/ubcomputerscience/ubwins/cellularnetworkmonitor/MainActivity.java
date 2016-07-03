aa61c204e012b8029890e167820ee4397511c4fb
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-03 00:06:00 -0400

    added user permissions, call to getCurrentDataState & getCurrentDataActivity methods ~ v1.4

--- app/src/main/java/ubcomputerscience/ubwins/cellularnetworkmonitor/MainActivity.java
+++ app/src/main/java/ubcomputerscience/ubwins/cellularnetworkmonitor/MainActivity.java
@@ -12,9 +12,11 @@
 
 package ubcomputerscience.ubwins.cellularnetworkmonitor;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
@@ -22,7 +24,9 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -31,6 +35,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.support.design.widget.Snackbar;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONObject;
 
@@ -50,15 +59,21 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener , ActivityCompat.OnRequestPermissionsResultCallback
 {
 
     public final String TAG = "[CELNETMON-ACTIVITY]";
+    protected GoogleApiClient mGoogleApiClient;
     Button track;
     DBstore dbStore;
     CellularDataRecorder cdr;
     LocationFinder locationFinder;
     Location location;
+    private View mLayout;
+    private static final int REQUEST_LOCATION = 0;
+    private static final int REQUEST_STORAGE = 1;
+    private static final int REQUEST_PHONE = 2;
 
     //Exports SQLiteDB to CSV file in Phone Storage
     public void exportToCSV()
@@ -86,7 +101,7 @@ public class MainActivity extends AppCompatActivity
                 DBHandler dbHandler = new DBHandler(getApplicationContext());
                 SQLiteDatabase sqLiteDatabase = dbHandler.getReadableDatabase();
                 Cursor curCSV = sqLiteDatabase.rawQuery("select * from cellRecords", null);
-                printWriter.println("Latitude,Longitude,locality,city,state,country,NETWORK_PROVIDER,TIMESTAMP,NETWORK_TYPE,NETWORK_STATE,NETWORK_RSSI");
+                printWriter.println("Latitude,Longitude,locality,city,state,country,NETWORK_PROVIDER,TIMESTAMP,NETWORK_TYPE,NETWORK_STATE,NETWORK_RSSI,DATA_STATE,DATA_ACTIVITY");
                 while(curCSV.moveToNext())
                 {
                     Double latitude = curCSV.getDouble(curCSV.getColumnIndex("LAT"));
@@ -101,8 +116,10 @@ public class MainActivity extends AppCompatActivity
                     String networkType = curCSV.getString(curCSV.getColumnIndex("NETWORK_TYPE"));
                     String networkState = curCSV.getString(curCSV.getColumnIndex("NETWORK_STATE"));
                     String networkRSSI = curCSV.getString(curCSV.getColumnIndex("NETWORK_RSSI"));
+                    String dataState = curCSV.getString(curCSV.getColumnIndex("DATA_STATE"));
+                    String dataActivity = curCSV.getString(curCSV.getColumnIndex("DATA_ACTIVITY"));
 
-                    String record = latitude + "," + longitude + "," + locality + "," + city + "," + stateName + "," + country + "," + networkProvider + "," + timeStamp + "," + networkType + "," + networkState + "," + networkRSSI;
+                    String record = latitude + "," + longitude + "," + locality + "," + city + "," + stateName + "," + country + "," + networkProvider + "," + timeStamp + "," + networkType + "," + networkState + "," + networkRSSI+ "," + dataState + "," + dataActivity;
                     Log.v(TAG, "attempting to write to file");
                     printWriter.println(record);
                     Log.v(TAG, "data written to file");
@@ -160,6 +177,50 @@ public class MainActivity extends AppCompatActivity
                 e.printStackTrace();
             }
         }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 
 
@@ -230,11 +291,15 @@ public class MainActivity extends AppCompatActivity
                         Log.v(TAG, "Calling getLocalTimeStamp and getCellularInfo");
                         String timeStamp = cdr.getLocalTimeStamp();
                         String cellularInfo = cdr.getCellularInfo(telephonyManager);
+                        String dataActivity = cdr.getCurrentDataActivity(telephonyManager);
+                        String dataState = cdr.getCurrentDataState(telephonyManager);
 
                         Log.v(TAG, "TIME STAMP: " + timeStamp);
                         Log.v(TAG, "CELLULAR INFO: " + cellularInfo);
+                        Log.v(TAG, "DATA ACTIVITY: "+dataActivity);
+                        Log.v(TAG, "DATA STATE: " +dataState);
                         dbStore = new DBstore(MainActivity.this);
-                        dbStore.insertIntoDB(location, timeStamp, cellularInfo);
+                        dbStore.insertIntoDB(location, timeStamp, cellularInfo,dataActivity, dataState);
 
                         locationFinder.addressResolver(location);
                         double latitude = location.getLatitude();
@@ -259,6 +324,152 @@ public class MainActivity extends AppCompatActivity
         catch (NullPointerException n)
         {
             n.printStackTrace();
+        }
+    }
+
+    private void requestLocationPermission() {
+        Log.i(TAG, "LOCATION permission has NOT been granted. Requesting permission.");
+
+        // BEGIN_INCLUDE(location_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+            Log.i(TAG,
+                    "Displaying location permission rationale to provide additional context.");
+            Snackbar.make(mLayout, R.string.permission_location_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    REQUEST_LOCATION);
+                        }
+                    })
+                    .show();
+        } else {
+
+            // Location permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+        }
+        // END_INCLUDE(location_permission_request)
+    }
+    private void requestPhonePermission() {
+        Log.i(TAG, "Phone permission has NOT been granted. Requesting permission.");
+
+        // BEGIN_INCLUDE(phone_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_PHONE_STATE)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+            Log.i(TAG,
+                    "Displaying Phone permission rationale to provide additional context.");
+            Snackbar.make(mLayout, R.string.permission_phone_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                                    REQUEST_PHONE);
+                        }
+                    })
+                    .show();
+        } else {
+
+            // Phone state permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE},
+                    REQUEST_PHONE);
+        }
+        // END_INCLUDE(phone_permission_request)
+    }
+    private void requestStoragePermission() {
+        Log.i(TAG, "STORAGE permission has NOT been granted. Requesting permission.");
+
+        // BEGIN_INCLUDE(storage_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+            Log.i(TAG,
+                    "Displaying location permission rationale to provide additional context.");
+            Snackbar.make(mLayout, R.string.permission_storage_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                    REQUEST_STORAGE);
+                        }
+                    })
+                    .show();
+        } else {
+
+            // Location permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE);
+        }
+        // END_INCLUDE(location_permission_request)
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_LOCATION) {
+            // BEGIN_INCLUDE(permission_result)
+            // Received permission result for location permission.
+            Log.i(TAG, "Received response for Location permission request.");
+
+            // Check if the only required permission has been granted
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Location permission has been granted
+                Log.i(TAG, "Location permission has now been granted.");
+                Snackbar.make(mLayout, R.string.permission_available_location,
+                        Snackbar.LENGTH_SHORT).show();
+            } else {
+                Log.i(TAG, "Location permission was NOT granted.");
+                Snackbar.make(mLayout, R.string.permissions_not_granted,
+                        Snackbar.LENGTH_SHORT).show();
+
+            }
+            // END_INCLUDE(permission_result)
+
+        } else if (requestCode == REQUEST_STORAGE) {
+            Log.i(TAG, "Received response for storage permissions request.");
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Storage permission has been granted
+                Log.i(TAG, "Storage permission has now been granted.");
+                Snackbar.make(mLayout, R.string.permission_available_storage,
+                        Snackbar.LENGTH_SHORT).show();
+            } else {
+                Log.i(TAG, "Storage permission was NOT granted.");
+                Snackbar.make(mLayout, R.string.permissions_not_granted,
+                        Snackbar.LENGTH_SHORT).show();
+
+            }
+        }
+        else if (requestCode == REQUEST_PHONE) {
+            Log.i(TAG, "Received response for phone permissions request.");
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Phone permission has been granted
+                Log.i(TAG, "Phone permission has now been granted.");
+                Snackbar.make(mLayout, R.string.permission_available_phone,
+                        Snackbar.LENGTH_SHORT).show();
+            } else {
+                Log.i(TAG, "Phone permission was NOT granted.");
+                Snackbar.make(mLayout, R.string.permissions_not_granted,
+                        Snackbar.LENGTH_SHORT).show();
+
+            }
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
