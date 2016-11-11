10eaa201e920a9decfc7436cc08a9402dfdf33ee
Abhishek Gautam <agautam2@buffalo.edu>
2016-11-10 19:55:33 -0500

    HTTP GET at every 3600 secs

--- app/src/main/java/edu/buffalo/cse/ubwins/cellmon/ScheduleIntentReceiver.java
+++ app/src/main/java/edu/buffalo/cse/ubwins/cellmon/ScheduleIntentReceiver.java
@@ -1,9 +1,29 @@
 package edu.buffalo.cse.ubwins.cellmon;
 
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
+import android.util.Base64;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
 
 /**
  * Created by Gautam on 7/18/16.
@@ -12,7 +32,7 @@ import android.util.Log;
  * University at Buffalo, The State University of New York.
  * Copyright © 2016 Gautam. All rights reserved.
  */
-public class ScheduleIntentReceiver
+public class ScheduleIntentReceiver extends Service
 {
     LocationFinder locationFinder;
     CellularDataRecorder cdr;
@@ -21,14 +41,16 @@ public class ScheduleIntentReceiver
     Location location;
     public final String TAG = "[CELNETMON-HNDLRCVR]";
     int keepAlive = 0;
+    String IMEI_HASH;
+    String IMEI;
  public void onScheduleIntentReceiver(Context arg0)
  {
      keepAlive++;
-     //Log.e(TAG, "KEEPALIVE: " + keepAlive);
-     //Log.v(TAG, "inside onReceive");
+
      locationFinder = new LocationFinder(arg0);
 
      final TelephonyManager telephonyManager = (TelephonyManager) arg0.getSystemService(Context.TELEPHONY_SERVICE);
+     IMEI = telephonyManager.getDeviceId();
      cdr = new CellularDataRecorder();
      pcsr = new PhoneCallStateRecorder();
 
@@ -65,14 +87,73 @@ public class ScheduleIntentReceiver
 
      dbStore = new DBstore(arg0);
      dbStore.insertIntoDB(locationdata, timeStamp, cellularInfo, dataActivity, dataState, phoneCallState, mobileNetworkType, locationProvider);
-
-
+     //Log.e(TAG, "KEEPALIVE: " + keepAlive);
      if(keepAlive == 1200)
      {
+         /*GET IMEI*/
+         try {
+             /*HASH EMEI*/
+             IMEI_HASH = genHash(IMEI);
+         }
+
+         catch(NoSuchAlgorithmException e)
+         {
+             e.printStackTrace();
+         }
+         Log.v(TAG, "GENERATED IMEI HASH");
+         //TODO KEEP-ALIVE GET
+         HttpResponse response = null;
+         try
+         {
+             HttpClient client = new DefaultHttpClient();
+             HttpGet request = new HttpGet();
+             String customURL = "http://104.196.177.7/aggregator/ping?imei_hash=" + URLEncoder.encode(IMEI_HASH, "UTF-8");
+             request.setURI(new URI(customURL));
+             response = client.execute(request);
+             Log.v(TAG, "RESPONSE PHRASE FOR HTTP GET: " + response.getStatusLine().getReasonPhrase());
+             Log.v(TAG, "RESPONSE STATUS FOR HTTP GET: " + response.getStatusLine().getStatusCode());
+         }
+         catch (URISyntaxException e)
+         {
+             e.printStackTrace();
+         }
+         catch (ClientProtocolException e)
+         {
+             e.printStackTrace();
+         }
+         catch (IOException e)
+         {
+             e.printStackTrace();
+         }
+         //RESET KEEPALIVE
          keepAlive = 0;
-         //TODO KEEP-ALIVE POST
+         //Log.e(TAG, "KEEPALIVE RESET: " + keepAlive);
      }
+
  }
+
+    private String genHash(String input) throws NoSuchAlgorithmException
+    {
+        String IMEI_Base64="";
+        try
+        {
+            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+            byte[] sha256Hash = sha256.digest(input.getBytes("UTF-8"));
+            IMEI_Base64 = Base64.encodeToString(sha256Hash, Base64.DEFAULT);
+            IMEI_Base64=IMEI_Base64.replaceAll("\n", "");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return IMEI_Base64;
+    }
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        // Used only in case of bound services.
+        return null;
+    }
 
 }
 
